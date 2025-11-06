0a53141f31d2a69eadb3181856c985d6b825c196
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-11-06 01:24:26 +0000

    Заинлайнила CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { type OutputAsset } from "rollup";
+import { type Plugin, defineConfig } from "vite";
 import { compression } from "vite-plugin-compression2";
 
 export default defineConfig(({ isSsrBuild }) => ({
@@ -7,6 +8,7 @@ export default defineConfig(({ isSsrBuild }) => ({
 
   plugins: [
     react(),
+    inlineIndexCSS(),
     isSsrBuild
       ? null
       : compression({
@@ -40,4 +42,44 @@ function splitChunks(id: string): string {
   }
 
   return "index";
+}
+
+function inlineIndexCSS(): Plugin {
+  return {
+    name: "inline-index-css",
+    apply: "build",
+    enforce: "post",
+    generateBundle(_, bundle) {
+      let htmlAsset: OutputAsset | undefined;
+      let cssAsset: OutputAsset | undefined;
+
+      for (const file of Object.values(bundle)) {
+        if (file.type === "asset" && file.fileName.endsWith(".html")) {
+          htmlAsset = file;
+        }
+
+        if (
+          file.type === "asset" &&
+          file.fileName.endsWith(".css") &&
+          file.fileName.includes("index")
+        ) {
+          cssAsset = file;
+        }
+      }
+
+      if (!htmlAsset || !cssAsset) {
+        return;
+      }
+
+      const html = String(htmlAsset.source);
+      const css = String(cssAsset.source);
+
+      htmlAsset.source = html.replace(
+        /<link[^>]+rel=["']stylesheet["'][^>]*>/,
+        `<style>${css}</style>`,
+      );
+
+      delete bundle[cssAsset.fileName];
+    },
+  };
 }
